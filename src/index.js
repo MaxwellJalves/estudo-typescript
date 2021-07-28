20eeda067df8d90959d0b1ffdebcf6046bee33ce
MaxwellJAlves <maxwell880@gmail.com>
2021-07-27 22:14:15 -0300

    Redirecionado os arquivos transpilados para a pasta dist, e ajustado o endpoint para receber uma string  + uma lista html

--- src/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var express = require("express");
-var app = express();
-//Router
-app.get('/', function (request, response) {
-    return response.send("Comunicação Realizada com sucesso!");
-});
-app.listen(3333);
